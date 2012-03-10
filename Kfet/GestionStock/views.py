4d0359afa138ca619c25e5686b09c4a339b9796f
Loïc Carney <elnox04@gmail.com>
2012-03-10 11:10:20 +0100

    Problème logo réglé et ignore des .swp

--- Kfet/GestionStock/views.py
+++ Kfet/GestionStock/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Gestion de stock index.")
+    return render_to_response('GestionStock/home.html', {})

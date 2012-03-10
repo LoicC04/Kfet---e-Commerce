6743347a7f1e0aed579425fbe6691851b6c931eb
Loïc Carney <elnox04@gmail.com>
2012-03-10 11:48:34 +0100

    Les slides passent seulement pour la page daccueil

--- Kfet/GestionStock/views.py
+++ Kfet/GestionStock/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
+from Kfet.Commun.models import Produit
 
 
 def index(request):
-    return render_to_response('GestionStock/home.html', {})
+    list_produit = Produit.objects.all()
+    return render_to_response('GestionStock/home.html', {'produits':list_produit})

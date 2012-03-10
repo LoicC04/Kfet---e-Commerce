61105e20c8de928877ce3a66b3345b2285049fcb
slacroix <erufu.sennin@gmail.com>
2012-03-10 11:42:44 +0100

    Detail produit dans module Ventes

--- Kfet/Ventes/urls.py
+++ Kfet/Ventes/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = patterns('Kfet.Ventes.views',
-    (r'^$', 'index'),
-    #(r'^(?P<poll_id>\d+)/$', 'detail'),
+
+    #url(r'^$', 'home'),
+    #(r'^$', 'index'),
+    url(r'^(?P<produit_id>\d+)/$', 'produit'),
 )
--- Kfet/Ventes/views.py
+++ Kfet/Ventes/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from Kfet.Commun.models import Produit
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Ventes index.")
+
+def produit(request, produit_id):
+#    p = get_object_or_404(Poll, pk=poll_id)
+    produit = Produit.objects.get(pk=produit_id)
+    return render_to_response('Ventes/produit.html', {'produit':produit})
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the Ventes index.")

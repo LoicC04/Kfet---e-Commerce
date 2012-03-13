cc8f256f629f9c99cbd86a5aab3c6fe0bdc9baff
slacroix <erufu.sennin@gmail.com>
2012-03-13 14:46:27 +0100

    If last dans le menu

--- Kfet/Ventes/views.py
+++ Kfet/Ventes/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from Kfet.Commun.models import Produit, Categorie
-
+from django.template import RequestContext
 
 def produit(request, produit_id):
 #    p = get_object_or_404(Poll, pk=poll_id)
     produit = Produit.objects.get(pk=produit_id)
-    return render_to_response('Ventes/produit.html', {'produit':produit})
+    return render_to_response('Ventes/produit.html', {'produit':produit}, context_instance=RequestContext(request))
 
 def categorie(request, cat_id):
 #    p = get_object_or_404(Poll, pk=poll_id)

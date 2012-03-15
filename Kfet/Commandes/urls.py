8ae2e9a0571f9ae337862b7519d4b39d45d35d19
slacroix <erufu.sennin@gmail.com>
2012-03-15 16:41:36 +0100

    Ventes => Commandes et suppression de ventes pour éviter l'enbtouille. /!\ Vérifier modules dans settings.py /!\

--- Kfet/Commandes/urls.py
+++ Kfet/Commandes/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = patterns('Kfet.Commandes.views',
-    (r'^$', 'index'),
-    #(r'^(?P<poll_id>\d+)/$', 'detail'),
+
+    #url(r'^$', 'home'),
+    #(r'^$', 'index'),
+    url(r'^(?P<produit_id>\d+)/$', 'produit'),
+    url(r'^panier/$', 'panier'),
+    url(r'^categorie/(?P<cat_id>\d+)/$', 'categorie'),
 )

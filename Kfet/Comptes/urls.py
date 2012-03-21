9a2a6e10f7428c4efcae4d243fde50fe2b71b4d3
slacroix <erufu.sennin@gmail.com>
2012-03-21 02:45:16 +0100

    Création regex et verif en cours blocage sur password et verif choix promo

--- Kfet/Comptes/urls.py
+++ Kfet/Comptes/urls.py
@@ -4,7 +4,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('Kfet.Comptes.views',
 
-    url(r'^creation/$', 'creation'),
+    url(r'^creation/(?P<erreur>\d+)$', 'creation'),
     url(r'^ajout/$', 'ajout'),
     url(r'^ok/$', 'ok'),
+    url(r'^test/$', 'test'),
+
 )
--- Kfet/Comptes/views.py
+++ Kfet/Comptes/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from Kfet.Commun.models import Promo, Personne
 from django.template import RequestContext
@@ -5,37 +7,53 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.utils.datastructures import MultiValueDictKeyError
 
+
 def ajout(request):
 
-    try: 
-        etu_post = request.POST['etu']
+    try:
+        request.session['etu'] = request.POST['etu']
     except MultiValueDictKeyError:
-        etu_post = "FUCK"
+        request.session['etu'] = ""
+    try:
+        request.session['nom'] = request.POST['nom']
+    except MultiValueDictKeyError:
+        request.session['nom'] = ""
+    try:
+        request.session['prenom'] = request.POST['prenom']
+    except MultiValueDictKeyError:
+        request.session['prenom'] = ""
+    try:
+        request.session['mail'] = request.POST['mail']
+    except MultiValueDictKeyError:
+        request.session['mail'] = ""
 
-    try: 
+    if request.POST['nom'] == "":
+        return HttpResponseRedirect('/comptes/creation/1')
+    else:
         nom_post = request.POST['nom']
-    except MultiValueDictKeyError:
-        nom_post = "FUCK"
 
-    try: 
+    if request.POST['etu'] == "":
+        return HttpResponseRedirect('/comptes/creation/3')
+    else:
+        etu_post = request.POST['etu']
+
+    if request.POST['prenom'] == "":
+        return HttpResponseRedirect('/comptes/creation/2')
+    else:
         prenom_post = request.POST['prenom']
-    except MultiValueDictKeyError:
-        prenom_post = "FUCK"
 
-    try: 
-        pass_post = request.POST['pass']
-    except MultiValueDictKeyError:
-        pass_post = "FUCK"
-
-    try: 
+    if request.POST['mail'] == "":
+        return HttpResponseRedirect('/comptes/creation/4')
+    else:
         mail_post = request.POST['mail']
-    except MultiValueDictKeyError:
-        mail_post = "FUCK"
 
-    try: 
+    if request.POST['promo'] == "":
+        return HttpResponseRedirect('/comptes/creation/5')
+    else:
         promo_post = request.POST['promo']
-    except MultiValueDictKeyError:
-        promo_post = 2
+
+    pass_post = request.POST['pass']
+
 
     personne = Personne(
         num_etu=etu_post,
@@ -48,9 +66,65 @@ def ajout(request):
     personne.save()
     return HttpResponseRedirect(reverse('Kfet.Comptes.views.ok'))
 
-def creation(request):
+def creation(request,erreur):
+    if 'etu' in request.session:
+        etu_post = request.session['etu']
+    else:
+        etu_post = ""
+    if 'nom' in request.session:
+        nom_post = request.session['nom']
+    else:
+        nom_post = ""
+    if 'prenom' in request.session:
+        prenom_post = request.session['prenom']
+    else:
+        prenom_post = ""
+    pass_post = ""
+    if 'mail' in request.session:
+        mail_post = request.session['mail']
+    else:
+        mail_post = ""
+    if 'promo' in request.session:
+        promo_post = request.session['promo']
+    else:
+        promo_post = ""
+
+    personne = Personne(
+        num_etu=etu_post,
+        nom=nom_post,
+        prenom=prenom_post,
+        password=pass_post,
+        mail=mail_post,
+        promo_id=promo_post
+    )
+
     promo = get_list_or_404(Promo)
-    return render_to_response('Comptes/creation.html' , {'promo':promo}, context_instance=RequestContext(request))
+    return render_to_response('Comptes/creation.html' , {'promo':promo, 'personne':personne, 'erreur':erreur}, context_instance=RequestContext(request))
 
 def ok(request):
+
+    try:
+        del request.session['etu']
+    except KeyError:
+        pass
+    try:
+        del request.session['nom']
+    except KeyError:
+        pass   
+    try:
+        del request.session['prenom']
+    except KeyError:
+        pass
+    try:
+        del request.session['mail']
+    except KeyError:
+        pass
     return HttpResponse("Hey")
+
+def test(request):
+        if 'count' in request.session:
+            request.session['count'] += 1
+            return HttpResponse('new count=%s' % request.session['count'])
+        else:
+            request.session['count'] = 1
+            return HttpResponse('No count in session. Setting to 1')

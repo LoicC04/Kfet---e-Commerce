1d426ffdf97d8f83ec0d555f527c517b9767601a
Loïc Carney <elnox04@gmail.com>
2012-03-24 18:19:30 +0100

    GestionStock : pagination ajoutée (même DOUBLE pagination!!!)

--- Kfet/GestionStock/views.py
+++ Kfet/GestionStock/views.py
@@ -9,7 +9,25 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):
     list_produit_rupture = Produit.objects.all().filter(quantite=0)
     list_produit_en_stock = Produit.objects.all().filter(quantite__gte=1)
-    return render_to_response('GestionStock/home.html', {'produits_out':list_produit_rupture, 'produits_in':list_produit_en_stock}, context_instance=RequestContext(request))
+
+    paginator_rupture = Paginator(list_produit_rupture, 1) # Show 10 items per page
+    paginator_stock = Paginator(list_produit_en_stock, 1) # Show 10 items per page
+
+    page_rupture = request.GET.get('pageRupture',1)
+    page_stock = request.GET.get('pageStock',1)
+    try:
+        produits_rupture = paginator_rupture.page(page_rupture)
+        produits_stock = paginator_stock.page(page_stock)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        produits_rupture = paginator_rupture.page(1)
+        produits_stock = paginator_stock.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        produits_rupture = paginator_rupture.page(paginator_rupture.num_pages)
+        produits_stock = paginator_stock.page(paginator_stock.num_pages)
+
+    return render_to_response('GestionStock/home.html', {'produits_out':produits_rupture, 'produits_in':produits_stock}, context_instance=RequestContext(request))
 
 def reappro(request):
     list_Fournisseur = Fournisseur.objects.all()

72ef435d92cba3c52556c0e1a9f937422b1176ba
Loïc Carney <elnox04@gmail.com>
2012-03-28 14:17:35 +0200

    corrections de droits d'accès sur choisir un menu

--- Kfet/views.py
+++ Kfet/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from Kfet.Commun.models.Menu  import *
 from django.shortcuts import get_object_or_404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -12,6 +13,7 @@ def listMenu(request):
     typeMenu = TypeMenu.objects.all()
     return render_to_response('Commandes/menus.html', {'typeMenu':typeMenu}, context_instance=RequestContext(request) ) 
 
+@login_required
 def choisirMenu(request,typeMenu_id, menu_id=None):
     typeMenu = get_object_or_404(TypeMenu, pk=typeMenu_id)
     if menu_id!=None:

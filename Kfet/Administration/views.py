74b6e14a230ba4f784af30c9306e5b109fbb9271
Loïc Carney <elnox04@gmail.com>
2012-04-10 14:38:24 +0200

    Suppression d'un TypeMenu terminé

--- Kfet/Administration/views.py
+++ Kfet/Administration/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from Kfet.Commun.models.TypeMenu import TypeMenu, TypeMenuForm
+from Kfet.Commun.models.Commande import Commande
 from django.shortcuts import render_to_response, HttpResponseRedirect, get_object_or_404
 from django.core.urlresolvers import reverse
 
@@ -37,14 +38,26 @@ def ajouterModifierMenu(request, menu_id=None):
 
 @login_required
 def supprimerMenu(request,menu_id):
-    menu = get_object_or_404(TypeMenu, pk=menu_id)
-    if menu.menu_set.count()==0:
-        menu.delete()
+    """ Fonction de suppression des menus de la carte de la Kfet """
+    # On récupère le typeMenu en question
+    typeMenu = get_object_or_404(TypeMenu, pk=menu_id)
+    if typeMenu.menu_set.count()==0:
+        # Si aucun Menu n'a été créé avec ce typeMenu alors on le supprime
+        typeMenu.delete()
         return  HttpResponseRedirect(reverse('Kfet.Administration.views.typeMenu'))
     else:
-        menu.actif=False
-        menu.save()
+        # Sinon, on vérifie pour chacun de ces menus, s'ils existent dans une commande. Si un meu n'existe pas dans une commande, c'est qu'il est dans le panier de quelqu'un
+        # Alors, on supprime ce menu du panier de l'utilisateur et on déactive ce TypeMenu
+        menus=typeMenu.menu_set.all()
+        for m in menus:
+            p = m.panier
+            if Commande.objects.filter(panier=p).count()==0:
+                m.delete()
+
+        typeMenu.actif=False
+        typeMenu.save()
         erreur=1
+        
         return  HttpResponseRedirect(reverse('Kfet.Administration.views.typeMenu', args=[erreur]))
 
 @login_required

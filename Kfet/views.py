f3827c030cf598594ed5af1a70e701888eca07f5
slacroix <erufu.sennin@gmail.com>
2012-04-10 15:15:39 +0200

    Debut recherche + Correction CSS de merde

--- Kfet/views.py
+++ Kfet/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from Kfet.Commun.models  import TypeMenu
+from Kfet.Commun.models  import TypeMenu, Produit, TypeMenu
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 def home(request):
@@ -11,6 +12,11 @@ def listMenu(request):
     typeMenu = TypeMenu.objects.filter(actif=True)
     return render_to_response('Commandes/menus.html', {'typeMenu':typeMenu}, context_instance=RequestContext(request) ) 
 
-@login_required
-def administration(request):
-    return render_to_response('Kfet/administration.html', {}, context_instance=RequestContext(request) ) 
+def recherche(request):    
+    if request.method == 'POST':
+        keyword = request.POST['keyword']
+        category = request.POST['category']
+        produit = Produit.objects.filter(nom__contains=keyword)
+        menu = TypeMenu.objects.filter(nom__contains=keyword)
+    return render_to_response('recherche.html', {'produit':produit, 'menu':menu}, context_instance=RequestContext(request) )
+
